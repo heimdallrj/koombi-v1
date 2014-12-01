9d23f00956912705cb167d1f344f940c99a8e59c
Ind <mailtokasun@gmail.com>
2014-12-01 16:10:12 +0530

    DB Update + Custom Post Type + Blog

--- wp-content/themes/kmbv1/home.php
+++ wp-content/themes/kmbv1/home.php
@@ -1,1 +1,31 @@
-#koombi Home
+<?php get_header(); ?>
+
+    <?php
+    global $wp_query;
+    $args = array_merge( $wp_query->query_vars, array( 'post_type' => 'project' ) );
+    query_posts( $args );
+    ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<div <?php post_class() ?> id="project-<?php the_ID(); ?>">
+
+			<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+
+			<div class="entry">
+				<?php the_content(); ?>
+			</div> <!--/.entry-->
+
+		</div> <!--/.project-->
+
+	<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<h4>Not Found</h4>
+
+	<?php endif; ?>
+    
+    <?php wp_reset_query(); // reset the query ?>
+
+<?php get_footer(); ?>
